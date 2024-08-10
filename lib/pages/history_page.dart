9aa822bfc42938a7c50dc14c6b44a9709e1e5b02
AGUN1505 <agun1505@gmail.com>
2024-08-10 20:59:47 +0700

    penambahan onrefresh

--- lib/pages/history_page.dart
+++ lib/pages/history_page.dart
@@ -7,120 +7,103 @@ import 'package:tour_and_travel/core/build_context_ext.dart';
 import 'package:tour_and_travel/pages/history_detail_page.dart';
 
 class HistoryPage extends StatelessWidget {
+  // Konstruktor untuk HistoryPage
   const HistoryPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Mengembalikan Scaffold yang berisi appBar dan body
     return Scaffold(
       appBar: AppBar(
+        // Mengatur judul appBar
         title: const Text('Riwayat Pemesanan'),
       ),
-      body: BlocConsumer<TransactionBloc, TransactionState>(
-        listener: (context, state) {
-          // if (state is TransactionFailed) {
-          //   context.showDialogError(state.message);
-          // }
+      body: RefreshIndicator(
+        // Fungsi untuk meng-refresh data ketika user menggesek ke bawah
+        onRefresh: () async {
+          // Menambahkan event untuk mengambil data transaksi ketika di-refresh
+          context.read<TransactionBloc>().add(const GetTransactionsEvent());
         },
-        builder: (context, state) {
-          if (state is TransactionLoading) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (state is TransactionSuccess) {
-            return ListView.separated(
-              padding: const EdgeInsets.all(16.0),
-              itemCount: state.data.length,
-              separatorBuilder: (context, index) => const Divider(),
-              itemBuilder: (context, index) {
-                final item = state.data[index];
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      DateFormat('dd MMMM yyyy', 'id_ID')
-                          .format(item.createdAt!),
-                      style: const TextStyle(
-                        fontSize: 12.0,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    ListTile(
-                      onTap: () => context.push(HistoryDetailPage(item: item)),
-                      leading: Assets.pesanTiketCircle.image(),
-                      title: Text(
-                        item.jurusan!.nama!,
+        child: BlocConsumer<TransactionBloc, TransactionState>(
+          // Mengkonsumsi state dan event dari TransactionBloc
+          listener: (context, state) {
+            // Menangani error jika terjadi kesalahan saat mengambil data transaksi
+            // if (state is TransactionFailed) {
+            //   context.showDialogError(state.message);
+            // }
+          },
+          builder: (context, state) {
+            // Menampilkan loading indicator jika sedang mengambil data
+            if (state is TransactionLoading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (state is TransactionSuccess) {
+              // Menampilkan daftar transaksi jika data berhasil diambil
+              return ListView.separated(
+                padding: const EdgeInsets.all(16.0),
+                itemCount: state.data.length,
+                separatorBuilder: (context, index) => const Divider(),
+                itemBuilder: (context, index) {
+                  final item = state.data[index];
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Menampilkan tanggal transaksi
+                      Text(
+                        DateFormat('dd MMMM yyyy', 'id_ID')
+                            .format(item.createdAt!),
                         style: const TextStyle(
-                          fontWeight: FontWeight.w600,
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.w500,
                         ),
                       ),
-                      subtitle: Row(
-                        children: [
-                          Icon(
-                            item.paidStatusIcon,
-                            color: item.paidStatusColor,
-                            size: 18.0,
+                      // Menampilkan detail transaksi
+                      ListTile(
+                        onTap: () =>
+                            context.push(HistoryDetailPage(item: item)),
+                        leading: Assets.pesanTiketCircle.image(),
+                        title: Text(
+                          item.jurusan!.nama!,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w600,
                           ),
-                          const SizedBox(width: 8.0),
-                          Text(
-                            item.paidStatus,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.w600,
+                        ),
+                        subtitle: Row(
+                          children: [
+                            Icon(
+                              item.paidStatusIcon,
+                              color: item.paidStatusColor,
+                              size: 18.0,
                             ),
-                          ),
-                        ],
+                            const SizedBox(width: 8.0),
+                            Text(
+                              item.paidStatus,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                      // subtitle: Column(
-                      //   crossAxisAlignment: CrossAxisAlignment.start,
-                      //   children: [
-                      //     Text(item.jurusan!.nama!),
-                      //     const Text('-'),
-                      //     Row(
-                      //       children: [
-                      //         const Icon(
-                      //           Icons.check_circle,
-                      //           color: AppColors.green,
-                      //           size: 18.0,
-                      //         ),
-                      //         const SizedBox(width: 8.0),
-                      //         Text(
-                      //           item.paidStatus,
-                      //           style: const TextStyle(
-                      //             fontWeight: FontWeight.w600,
-                      //           ),
-                      //         ),
-                      //       ],
-                      //     ),
-                      //   ],
-                      // ),
-                      // trailing: Text(
-                      //   item.price.currencyFormatRp,
-                      //   style: const TextStyle(
-                      //     fontSize: 14.0,
-                      //   ),
-                      // ),
-                    ),
-                  ],
-                );
-              },
+                    ],
+                  );
+                },
+              );
+            }
+            // Menampilkan pesan jika tidak ada data transaksi
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Assets.empty.image(width: 150.0),
+                  const SizedBox(height: 20.0),
+                  const Text('Riwayat pemesanan kosong..'),
+                ],
+              ),
             );
-          }
-          return Center(
-            // child: IconButton(
-            //   onPressed: () => context
-            //       .read<TransactionBloc>()
-            //       .add(const GetTransactionsEvent()),
-            //   icon: const Icon(Icons.refresh),
-            // ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Assets.empty.image(width: 150.0),
-                const SizedBox(height: 20.0),
-                const Text('Riwayat pemesanan kosong..'),
-              ],
-            ),
-          );
-        },
+          },
+        ),
       ),
     );
   }
--- lib/pages/ticket_list_page.dart
+++ lib/pages/ticket_list_page.dart
@@ -1,103 +1,121 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:intl/intl.dart';
-import 'package:tour_and_travel/bloc/get_tickets/get_tickets_bloc.dart';
-import 'package:tour_and_travel/core/assets.gen.dart';
-import 'package:tour_and_travel/core/build_context_ext.dart';
-import 'package:tour_and_travel/core/colors.dart';
-import 'package:tour_and_travel/core/num_ext.dart';
-import 'package:tour_and_travel/pages/create_order_page.dart';
+import 'package:flutter/material.dart'; // Import library untuk membuat aplikasi Flutter
+import 'package:flutter_bloc/flutter_bloc.dart'; // Import library untuk menggunakan Flutter Bloc
+import 'package:intl/intl.dart'; // Import library untuk mengatur format tanggal dan waktu
+import 'package:tour_and_travel/bloc/get_tickets/get_tickets_bloc.dart'; // Import library untuk mengatur logika bisnis tiket
+import 'package:tour_and_travel/core/assets.gen.dart'; // Import library untuk mengatur aset aplikasi
+import 'package:tour_and_travel/core/build_context_ext.dart'; // Import library untuk memperluas konteks build
+import 'package:tour_and_travel/core/colors.dart'; // Import library untuk mengatur warna aplikasi
+import 'package:tour_and_travel/core/num_ext.dart'; // Import library untuk memperluas fungsi numerik
+import 'package:tour_and_travel/pages/create_order_page.dart'; // Import library untuk halaman buat pesanan
 
 class TicketListPage extends StatelessWidget {
-  const TicketListPage({super.key});
+  const TicketListPage({super.key}); // Konstruktor untuk TicketListPage
 
   @override
   Widget build(BuildContext context) {
+    // Menambahkan event untuk mendapatkan tiket ketika halaman dibuka
     context.read<GetTicketsBloc>().add(const DoGetTicketsEvent());
     return Scaffold(
-      backgroundColor: AppColors.background,
+      backgroundColor: AppColors.background, // Mengatur warna latar belakang
       appBar: AppBar(
-        title: const Text('Daftar Tiket'),
+        title: const Text('Daftar Tiket'), // Mengatur judul app bar
       ),
-      body: ListView(
-        children: [
-          ListTile(
-            tileColor: AppColors.white,
-            title: const Text('Tanggal Hari Ini'),
-            subtitle: Text(
-              DateFormat('EEEE, dd MMMM yyyy', 'id_ID').format(DateTime.now()),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          // Menambahkan event untuk mendapatkan tiket ketika halaman di-refresh
+          context.read<GetTicketsBloc>().add(const DoGetTicketsEvent());
+        },
+        child: ListView(
+          children: [
+            ListTile(
+              tileColor:
+                  AppColors.white, // Mengatur warna latar belakang list tile
+              title: const Text('Tanggal Hari Ini'), // Mengatur judul list tile
+              subtitle: Text(
+                DateFormat('EEEE, dd MMMM yyyy', 'id_ID').format(
+                    DateTime.now()), // Mengatur format tanggal dan waktu
+              ),
             ),
-          ),
-          const SizedBox(height: 8.0),
-          BlocConsumer<GetTicketsBloc, GetTicketsState>(
-            listener: (context, state) {
-              if (state is GetTicketsFailed) {
-                context.showDialogError(state.message);
-              }
-            },
-            builder: (context, state) {
-              if (state is GetTicketsLoading) {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              } else if (state is GetTicketsSuccess) {
-                return ListView.separated(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: state.data.length,
-                  separatorBuilder: (context, index) =>
-                      const SizedBox(height: 2.0),
-                  itemBuilder: (context, index) {
-                    final item = state.data[index];
-                    return ListTile(
-                      onTap: () {
-                        context.push(CreateOrderPage(
-                          item: item,
-                        ));
-                      },
-                      tileColor: AppColors.white,
-                      leading: Assets.icBus.image(),
-                      title: Text(item.mobil!.namaMobil!),
-                      subtitle: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Jambi > ${item.kota!.namaKota!} '),
-                          Text(item.jam!),
-                        ],
-                      ),
-                      trailing: RichText(
-                        text: TextSpan(
-                          text: item.harga!.currencyFormatRp,
-                          style: const TextStyle(
-                            fontSize: 14.0,
-                            color: AppColors.yellow,
-                          ),
-                          children: const [
-                            TextSpan(
-                              text: '/seat',
-                              style: TextStyle(
-                                fontSize: 11.0,
-                                color: AppColors.black,
-                              ),
-                            ),
+            const SizedBox(height: 8.0), // Mengatur jarak antar widget
+            BlocConsumer<GetTicketsBloc, GetTicketsState>(
+              listener: (context, state) {
+                // Menangani error jika terjadi kesalahan saat mendapatkan tiket
+                if (state is GetTicketsFailed) {
+                  context.showDialogError(state.message);
+                }
+              },
+              builder: (context, state) {
+                // Menampilkan loading indicator jika sedang mengambil data
+                if (state is GetTicketsLoading) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (state is GetTicketsSuccess) {
+                  // Menampilkan daftar tiket jika data berhasil diambil
+                  return ListView.separated(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: state.data.length,
+                    separatorBuilder: (context, index) => const SizedBox(
+                        height: 2.0), // Mengatur jarak antar item
+                    itemBuilder: (context, index) {
+                      final item = state.data[index];
+                      return ListTile(
+                        onTap: () {
+                          // Menavigasi ke halaman buat pesanan ketika item diklik
+                          context.push(CreateOrderPage(
+                            item: item,
+                          ));
+                        },
+                        tileColor: AppColors
+                            .white, // Mengatur warna latar belakang list tile
+                        leading: Assets.icBus.image(), // Mengatur gambar bus
+                        title: Text(
+                            item.mobil!.namaMobil!), // Mengatur judul mobil
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                                'Jambi > ${item.kota!.namaKota!} '), // Mengatur rute
+                            Text(
+                                'Jam Keberangkatan > ${item.jam!}'), // Mengatur jam keberangkatan
                           ],
                         ),
-                      ),
-                    );
-                  },
+                        trailing: RichText(
+                          text: TextSpan(
+                            text: item.harga!
+                                .currencyFormatRp, // Mengatur format harga
+                            style: const TextStyle(
+                              fontSize: 14.0,
+                              color: AppColors.yellow,
+                            ),
+                            children: const [
+                              TextSpan(
+                                text: '/seat', // Mengatur satuan harga
+                                style: TextStyle(
+                                  fontSize: 11.0,
+                                  color: AppColors.black,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                }
+                // Menampilkan tombol refresh jika tidak ada data atau terjadi error
+                return Center(
+                  child: IconButton(
+                    onPressed: () => context.read<GetTicketsBloc>().add(
+                        const DoGetTicketsEvent()), // Menambahkan event untuk mendapatkan tiket
+                    icon: const Icon(Icons.refresh), // Mengatur icon refresh
+                  ),
                 );
-              }
-              return Center(
-                child: IconButton(
-                  onPressed: () => context
-                      .read<GetTicketsBloc>()
-                      .add(const DoGetTicketsEvent()),
-                  icon: const Icon(Icons.refresh),
-                ),
-              );
-            },
-          ),
-        ],
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
